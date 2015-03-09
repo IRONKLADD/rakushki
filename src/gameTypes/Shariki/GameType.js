1d7d481bf90228234abc4810b2fcb5eefb96f24d
Dan Wysocki <dwysocki@oswego.edu>
2015-03-09 11:08:22 -0400

    Further fleshed out SharikiGameType, and added comments

--- src/gameTypes/Shariki/GameType.js
+++ src/gameTypes/Shariki/GameType.js
@@ -1,19 +1,65 @@
 function SharikiGameType(players, config) {
+    /**
+     * Whether there is an active (selected) shell.
+     * @type {boolean}
+     */
+    this._isActive = false;
+    /**
+     * The row of the active shell.
+     * @type {number}
+     */
     this._activeRow = null;
+    /**
+     * The column of the active shell.
+     * @type {number}
+     */
     this._activeCol = null;
-    
+
+    /**
+     * Whether there is a valid swap at this moment in time.
+     * @type {boolean}
+     */
+    this._validSwap = false;
+
+    /**
+     * Constructs a Board object using the constraints imposed by Shariki
+     * and the configuration, and stores it in this._board.
+     */
     this._makeBoard = function() {
         this._board = new Board(this._config.width, this._config.height);
         this._fillBoard(this._board);
     }
 
+    /**
+     * Executes a single turn of the game. While waiting for the user to make
+     * a move, a background task is run to check if the board is in the
+     * game-over state (see this._checkBoard()). Returns whether or not the
+     * game is in fact over at the beginning of the turn.
+     *
+     * @param {Player} currentPlayer The current player (unused)
+     * @param {number} turnCount The current turn number (unused)
+     */
     this._turn = function(currentPlayer, turnCount) {
-        
+        var gameOver = this._checkBoard();
+        if(gameOver)
+            return true;
+        while(!this._validSwap) {
+            // wait T milliseconds
+        }
+        this._validSwap = false;
+        return false;
     }
 
+    /**
+     * This method handles the functionality of selecting a single shell.
+
+     * @param {number} row The row of the selected shell
+     * @param {number} col The column of the selected shell
+     */
     this.selectShell = function(row, col) {
         // no shell is active, make selected shell active
-        if(this._activeRow === null && this._activeCol === null) {
+        if(!this._isActive) {
+            this._isActive = true;
             this._activeRow = row;
             this._activeCol = col;
             // notify renderer here
@@ -24,12 +70,16 @@ function SharikiGameType(players, config) {
         }
         // unselect shell
         else {
+            this._isActive = false;
             this._activeRow = null;
             this._activeCol = null;
             // notify renderer here
         }
     }
 
+    /**
+     * Attempts to make a swap.
+     */
     this._trySwap = function(activeRow, activeCol, selectedRow, selectedCol) {
         this._swap(activeRow, activeCol, selectedRow, selectedCol);
         // notify renderer (in parallel)
@@ -47,6 +97,12 @@ function SharikiGameType(players, config) {
      * Checks if the 2 swapped shells make a connection
      * If they do, clear the connected shells, give points, and make new
      * shells fall in.
+     *
+     * @param  {number} activeRow Row of active shell.
+     * @param  {number} activeCol Column of active shell.
+     * @param  {number} selectedRow Row of selected shell.
+     * @param  {number} selectedCol Column of selected shell.
+     * @return {boolean} Whether or not the swap was valid.
      */
     this._makeConnection = function(activeRow, activeCol,
                                     selectedRow, selectedCol) {
@@ -56,6 +112,14 @@ function SharikiGameType(players, config) {
         // ...
     }
 
+    /**
+     * Swaps two shells.
+     *
+     * @param  {number} activeRow Row of active shell.
+     * @param  {number} activeCol Column of active shell.
+     * @param  {number} selectedRow Row of selected shell.
+     * @param  {number} selectedCol Column of selected shell.
+     */
     this._swap = function(activeRow, activeCol, selectedRow, selectedCol) {
         var activeShell = this._board.get(activeRow, activeCol);
         var selectedShell = this._board.get(selectedRow, selectedCol);
@@ -64,5 +128,16 @@ function SharikiGameType(players, config) {
         this._board.set(activeRow, activeCol, selectedShell);
     }
 
+    /**
+     * Checks to see if the board is in the game-over position. This means that
+     * there is no possible way to connect 3 or more with a single swap. If at
+     * any point during execution, the user makes a swap, obviously it was
+     * possible, and false is returned.
+     *
+     * @return {boolean} Whether or not the board is in game-over position.
+     */
+    this._checkBoard = function() {
+        /* ... */
+    }
 }
 // make this extend GameType
