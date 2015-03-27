195f116ea974245eaa42ebc80cba4f36332d663d
Dan Wysocki <dwysocki@oswego.edu>
2015-03-27 09:54:48 -0400

    Wrote helper function for building test boards

--- src/core/Main.js
+++ src/core/Main.js
@@ -1,22 +1,24 @@
+function boardFromArray(array, height, width) {
+    var board = new Board(height, width);
+    for(var row = 0; row < height; row++)
+        for(var col = 0; col < width; col++)
+            board.set(row, col,
+                      new Shell(array[row][col], null,
+                                Shariki.NORMALSHELL, null));
+    return board;
+}
+
+var board1 = [
+    ["blue", "green", "blue"],
+    ["red", "blue", "red"],
+    ["blue", "yellow", "blue"]
+];
+
 var app = Cut(function(root,container) {
     var config = new Configuration();
     var player1 = new Player();
 
-    var board = new Board(3, 3);
-    var insertShell = function(row, col, color) {
-        board.set(row, col,
-                  new Shell(color, null, Shariki.NORMALSHELL, null));
-    };
-    insertShell(0, 0, "blue");
-    insertShell(0, 1, "green");
-    insertShell(0, 2, "blue");
-    insertShell(1, 0, "red");
-    insertShell(1, 1, "blue");
-    insertShell(1, 2, "red");
-    insertShell(2, 0, "blue");
-    insertShell(2, 1, "yellow");
-    insertShell(2, 2, "blue");
-
+    var board = boardFromArray(board1, 3, 3);
     player1.setBoard(board);
 
     var players = [player1];
