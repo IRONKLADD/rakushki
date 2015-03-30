15d5ed4d1d14ee94be21547f69f5e64b104aef21
Dan Wysocki <dwysocki@oswego.edu>
2015-03-30 03:06:23 -0400

    Cleaned up a lot of code and fixed a bug

--- src/core/Util.js
+++ src/core/Util.js
@@ -1,52 +1,44 @@
-/*
- * A class to hold static functions
+/**
+ * A class to hold utility functions
  */
+function Util() {}
+/**
+ * Checks if two Shells in the grid are adjacent.
+ * @param row1 {int} The row corresponding to the first Shell.
+ * @param col1 {int} The column corresponding to the first Shell.
+ * @param row2 {int} The row corresponding to the second Shell.
+ * @param col2 {int} The column corresponding to the second Shell.
+ * @return {boolean} Returns true if the Shells are adjacent.
+ *
+ * This should be a static function when finished.
+ */
+Util.isAdjacent = function(row1, col1, row2, col2) {
+    var rowDiff = row2 - row1;
+    var colDiff = col2 - col1;
 
+    return rowDiff === 0 && Math.abs(colDiff) === 1
+        || colDiff === 0 && Math.abs(rowDiff) === 1;
+};
 
-    /**
-     * Checks if two Shells in the grid are adjacent.
-     * @param row1 {int} The row corresponding to the first Shell.
-     * @param col1 {int} The column corresponding to the first Shell.
-     * @param row2 {int} The row corresponding to the second Shell.
-     * @param col2 {int} The column corresponding to the second Shell.
-     * @return {boolean} Returns true if the Shells are adjacent.
-     *
-     * This should be a static function when finished.
-     */
-    var isAdjacent = function(row1, col1, row2, col2) {
-        console.log("In adj");
-        console.log(row1);
-        console.log(col1);
-        console.log(row2);
-        console.log(col2);
-        var rowDiff = row2 - row1;
-        var colDiff = col2 - col1;
-        if(rowDiff === 0) {
-            console.log("first call " +(colDiff === -1 || colDiff === 1));
-            return (colDiff === -1 || colDiff === 1);
-        }
-        else if(colDiff === 0) {
-            console.log("second call " +(rowDiff === -1 || colDiff === 1));
-            return (rowDiff === -1 || colDiff === 1);
-        }   
-        console.log("3rd call false");
-        return false;
-    }
+Util.setUnion = function(A, B){
+    var union = new Set([]);
+    A.forEach(function(element) { union.add(element) });
+    B.forEach(function(element) { union.add(element) });
 
-    this.setUnion = function(setA, setB){
-        var unionSet = new Set([]);
-        var setIterA = setA.values();
-        var setIterB = setB.values();
-        for(var i = 0; i < setA.size; i++){
-            unionSet.add(setIterA.next().value);
-        }
-        for(var i = 0; i < setB.size; i++){
-            unionSet.add(setIterB.next().value);
-        }
-        return unionSet;
+    return union;
+};
 
+Util.randomInt = function(limit) {
+    return Math.floor(Math.random() * limit);
+};
 
-    } 
+Util.Coord = function(row, col) {
+    this.row = row;
+    this.col = col;
+};
 
-
-
+Util.indexToCoord = function(index, height) {
+    var row = Math.floor(index / height);
+    var col = (index % height);
+    return new Util.Coord(row, col);
+};
--- src/gameTypes/Shariki/Display.js
+++ src/gameTypes/Shariki/Display.js
@@ -1,8 +1,6 @@
-function Display(root,players,config1){
+function Display(root,players,config){
     this._createBoard = _createBoard;
-    this._arrayCoord  = _arrayCoord;
     this.update       = update;
-    var config = config1;
     var board = players[0].getBoard();
     var gameScreen = Cut.create();
     gameScreen.appendTo(root).pin("align", .5);
@@ -14,16 +12,15 @@ function Display(root,players,config1){
     });
     var column;
     _createBoard(players[0]);
-    
 
     function _createBoard(player) {
         var j = 0, i = 0,count = 0;
         var boardNode = Cut.create();
         boardNode.appendTo(gameScreen);
         column = Cut.column().appendTo(root).pin("align", .5).spacing(1);
-        for (i = 0; i < config.getHeight(); i++) {
+        for (i = 0; i < config.height; i++) {
             var row = Cut.row().appendTo(column).spacing(1);
-            for (j = 0; j < config.getWidth(); j++) {
+            for (j = 0; j < config.width; j++) {
                 // colors as frames
                 var temp = board.get(i,j);
                 var cell = Cut.image("base:color_" + temp.color).appendTo(row);
@@ -32,19 +29,14 @@ function Display(root,players,config1){
                 count++;
                 //userInput.setInput(cell);
                 cell.on(Cut.Mouse.CLICK,function(point) {
-                    var coord = _arrayCoord(this._index);
-                    player.selectShell(coord[0],coord[1]);
+                    var coord = Util.indexToCoord(this._index, config.height);
+                    player.selectShell(coord.row, coord.col);
                     player.getBoard().printArr();
                 });
             }
         }
     }
-    function _arrayCoord(index) {
-        var row = Math.floor(index / config.getHeight());
-        var col = (index % config.getHeight());
-        var results = [row,col];
-        return results;
-    }
+
     function update() {
         column.remove();
         _createBoard();
