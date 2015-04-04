ae21b081993cfea626cf1f97ffac39565ea7a077
Dan Wysocki <dwysocki@oswego.edu>
2015-04-03 21:33:25 -0400

    Renamed ascii_*.png to ascii_nimbus_*.png to make room for new fonts

--- src/resources/ascii.js
+++ src/resources/ascii.js
@@ -3,7 +3,7 @@ colors = ["black", "white", "red", "blue", "yellow", "green"];
 colors.forEach(function(color) {
     Cut({
         name       : "ascii_nimbus_" + color,
-        imagePath  : "src/resources/ascii_" + color + ".png",
+        imagePath  : "src/resources/ascii_nimbus_" + color + ".png",
         imageRatio : 0,
         trim       : 0,
         cutouts    : Sprites.cutoutsFromGrid(Sprites.ascii, "",
