701694187498cd337d754da429003a6cf81bab54
Dan Wysocki <dwysocki@oswego.edu>
2015-04-03 21:11:40 -0400

    Added sprites for all ASCII characters in Nimbus-Mono font

---
+++ src/resources/ascii.js
@@ -0,0 +1,8 @@
+Cut({
+    name       : "ascii_nimbus",
+    imagePath  : "src/resources/ascii.png",
+    imageRatio : 0,
+    trim       : 0,
+    cutouts    : Sprites.cutoutsFromGrid(Sprites.ascii, "",
+                                         1381, 232, 32, 3)
+});
